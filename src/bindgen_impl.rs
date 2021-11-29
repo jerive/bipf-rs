bfd372bd2690a27ced177e1e865aef2a7e5dd962
Jerome Viveret <jerome.viveret@gmail.com>
2021-11-29 21:40:51 +0100

    Add a clue

--- src/bindgen_impl.rs
+++ src/bindgen_impl.rs
@@ -4,15 +4,13 @@ use node_bindgen::derive::node_bindgen;
 use node_bindgen::sys::*;
 use node_bindgen::core::NjError;
 use node_bindgen::core::TryIntoJs;
-use node_bindgen::core::buffer::*;
 use node_bindgen::core::val::*;
 use node_bindgen::core::buffer::ArrayBuffer;
 use integer_encoding::*;
 
 #[node_bindgen(name="decode")]
-fn bindgen_decode(value: JSArrayBuffer, start: f64, env: JsEnv) -> Result<napi_value, NjError> {
-    let start = start as usize;
-    decode_rec_bindgen(env, &value, start as usize)
+fn bindgen_decode(value: &[u8], start: f64, env: JsEnv) -> Result<napi_value, NjError> {
+    decode_rec_bindgen(env, value, start as usize)
 }
 
 pub fn decode_rec_bindgen(
@@ -81,6 +79,23 @@ pub fn decode_string_bindgen(
     start: usize,
     len: usize,
 ) -> Result<napi_value, NjError> {
+    
+    /*
+     * I see no difference between this piece of code and the one actually executed, below. Do you?
+     * (it is the near exact replication of cx.create_string_utf8_from_bytes).
+     * Yet it makes the perf test run 5 times slower (like zig).
+     * Made me think of https://github.com/staltz/bipf-native/blob/d69f72193f2f02e938e2faf9037a6a7df784cbc7/src/decode.zig#L11
+     * Could there be anything there...?
+     */
+    // let mut js_value = std::ptr::null_mut();
+    // let str = &buf[start.. start + len];
+    // unsafe {
+    //     napi_create_string_utf8(cx.inner(), 
+    //         str.as_ptr() as *const ::std::os::raw::c_char, 
+    //         buf.len(), &mut js_value)
+    // };
+    // Ok(js_value)
+
     cx.create_string_utf8_from_bytes(&buf[start..start + len])
 }
 
@@ -154,7 +169,6 @@ pub fn decode_object_bindgen(
 ) -> Result<napi_value, NjError>{
     let mut c = 0;
     let obj = cx.create_object()?;
-    let napi_env = cx.inner();
 
     while c < len {
         let decoded: Option<(usize, usize)> = VarInt::decode_var(&buf[start + c..]);
@@ -190,7 +204,7 @@ pub fn decode_object_bindgen(
         c += len;
         
         unsafe {
-            napi_set_named_property(napi_env, obj, key.as_ptr(), value);
+            napi_set_named_property(cx.inner(), obj, key.as_ptr(), value);
         }        
     }
 
